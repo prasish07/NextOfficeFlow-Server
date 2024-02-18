850ca4ad120dfab378b7fba400eadf601e1e83a5
prasish07 <prasishshrestha0099@gmail.com>
2024-02-18 12:25:59 +0545

    Add request api routes

---
+++ src/routes/request.ts
@@ -0,0 +1,32 @@
+import { Router, Request, Response } from "express";
+
+import {
+	authorizePermission,
+	validateToken,
+} from "../middleware/auth.middleware";
+
+const router = Router();
+
+import {
+	createRequest,
+	deleteRequest,
+	getAllRequests,
+	getRequest,
+	getRequests,
+	updateRequest,
+} from "../controllers/request.controller";
+
+router
+	.route("/request")
+	.post(validateToken, createRequest)
+	.get(validateToken, authorizePermission("admin", "HR"), getRequests);
+
+router
+	.route("/request/:requestId")
+	.get(validateToken, getRequest)
+	.put(validateToken, authorizePermission("admin", "HR"), updateRequest)
+	.delete(validateToken, authorizePermission("admin", "HR"), deleteRequest);
+
+router
+	.route("/requests")
+	.get(validateToken, authorizePermission("admin", "HR"), getAllRequests);
